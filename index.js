bd73159cc89911c0767ca7088078b512755a62cc
Erick_Tufiño <eato2001@hotmail.com>
2025-12-16 05:50:51 -0500

    Deploy

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const port=4017;
+require('dotenv').config();
+
+const port = process.env.PORT || 4017;
+
+
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb+srv://eatufino1:eatufino1@cluster0.o2uzj25.mongodb.net/MedicalAppointment?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_URI);
 
 const db=mongoose.connection;
 db.on('error', (error)=> console.error(error));
